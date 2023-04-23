d46069c514c619b0df5310dd80fa0c00e5275a67
Suriyaprakash B <128825289+suriyaprakashB@users.noreply.github.com>
2023-04-23 15:11:21 +0530

    Add files via upload

--- Square number/Square number.js
+++ Square number/Square number.js
@@ -1,1 +1,8 @@
+/*16-04-2023
+Write a program that takes an array of numbers and returns a new array with each number squared. */
 
+const inputArr = [2, 4, 6, 8, 10];
+let squaredArr = [];
+for (let i = 0; i < inputArr.length; i++) 
+squaredArr.push(inputArr[i] ** 2);
+console.log("The squared number is: "+ squaredArr);
